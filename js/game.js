ae2c9eb677947670ce2b2af411bee2ce797c8cb1
mih <mih@pop-os.localdomain>
2019-12-27 17:15:06 +0200

    Moved variable initialization in a fucntion

--- js/game.js
+++ js/game.js
@@ -1,37 +1,6 @@
 var canvas = document.getElementById("gamecanvas");
 var context = canvas.getContext("2d");
 
-var gameObjects = [];
-var speed = 5; // horizontal movement speed, should increase once every x distance
-var speedIncrement = 0.01; // the amount of speed that should be increased
-var speedIncrementDistance = 10;
-
-var distance = 0;
-var distanceUntilSpeedIncrement = speedIncrementDistance;
-
-var debugParagraph = document.getElementById("p-debug");
-
-player = new Player(
-    new Vector2(50, 100),
-    new Vector2(128, 128),
-    new Sprite("/assets/sprites/spritesheet.png", new Vector2(0, 0), new Vector2(128, 128)),
-    null,
-    20,
-    canvas.clientHeight - 128 - 20
-);
-player.collider = new CircleCollider(player.getCenter(), new Vector2(0, -3), 55);
-
-gameObjects.push(player);
-
-var enemySpawner = new EnemySpawner(
-    canvas.clientWidth + 200,
-    20,
-    canvas.clientHeight - 128 - 20,
-    1,
-    3,
-    speed
-);
-
 window.addEventListener(
     "keydown",
     function(event) {
@@ -52,9 +21,40 @@ window.addEventListener(
     true
 );
 
-var lastTick = performance.now();
-var thisTick = performance.now();
-var deltaTime = thisTick - lastTick;
+function init() {
+    gameObjects = [];
+    speed = 5; // horizontal movement speed, should increase once every x distance
+    speedIncrement = 0.01; // the amount of speed that should be increased
+    speedIncrementDistance = 10;
+
+    distance = 0;
+    distanceUntilSpeedIncrement = speedIncrementDistance;
+
+    player = new Player(
+        new Vector2(50, 100),
+        new Vector2(128, 128),
+        new Sprite("/assets/sprites/spritesheet.png", new Vector2(0, 0), new Vector2(128, 128)),
+        null,
+        20,
+        canvas.clientHeight - 128 - 20
+    );
+    player.collider = new CircleCollider(player.getCenter(), new Vector2(0, -3), 55);
+
+    gameObjects.push(player);
+
+    enemySpawner = new EnemySpawner(
+        canvas.clientWidth + 200,
+        20,
+        canvas.clientHeight - 128 - 20,
+        1,
+        3,
+        speed
+    );
+
+    lastTick = performance.now();
+    thisTick = performance.now();
+    deltaTime = thisTick - lastTick;
+}
 
 function tick() {
     lastTick = thisTick;
@@ -74,6 +74,8 @@ function updateDistance() {
         enemySpawner.speed = speed;
     }
 }
+
+var debugParagraph = document.getElementById("p-debug");
 
 function printDebugInfo() {
     debugParagraph.innerHTML = "Distance: " + Math.floor(distance) + "<br>Speed: " + speed;
@@ -124,4 +126,9 @@ function gameloop() {
     requestAnimationFrame(gameloop);
 }
 
-canvas.onload = gameloop();
+function start() {
+    init();
+    gameloop();
+}
+
+canvas.onload = start();
